4c51665def21e439efe327c0f3c144fbd3f826b6
aguspcaro <aguspcaro@gmail.com>
2021-03-11 20:00:44 -0300

    notas basico terminado

--- public/js/script.js
+++ public/js/script.js
@@ -9,14 +9,44 @@
     //Funciones
 
     var agregarTarea = function() {
-        alert("Agregar tarea")
-    };
-    var comprobarInput = function() {
-        alert("Comprobar tarea")
-    };
-    var eliminarTarea = function() {
-        alert("Eliminar tarea")
+        let tarea = tareaInput.value,
+            nuevaTarea = document.createElement("li"),
+            enlace = document.createElement("a")
+            contenido = document.createTextNode(tarea);
 
+        if(tarea === "") {
+            tareaInput.setAttribute("placeholder", "Agregar una tarea");
+            tareaInput.className = "error";
+            return false;
+        }
+
+        // Agregamos el contenido al enlace
+        enlace.appendChild(contenido);
+        // Le establecemos un atributo href
+        enlace.setAttribute("href", "#");
+        // Agregamo el enlace (a) a la nuevaa tarea
+        nuevaTarea.appendChild(enlace);
+        // Agregamos la nueva tarea a la lista
+        lista.appendChild(nuevaTarea)
+
+        tareaInput.value = "";
+
+        for (let i = 0; i <= lista.children.length -1; i++) {
+            lista.children[i].addEventListener("click", function(){
+                this.parentNode.removeChild(this)
+            });
+        }
+
+
+    };
+
+    var comprobarInput = function() {
+        tareaInput.className = "";
+        tareaInput.setAttribute("placeholder", "Agrear una tarea")
+    };
+
+    var eliminarTarea = function() {
+        this.parentNode.removeChild(this)
     };
 
 
